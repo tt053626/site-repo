d0fe9055f3d5ee406e1f41843e1344af46e26673
tt053626 <chihyuchen2@gmail.com>
2023-03-06 21:14:58 +0100

    update frontpage-3, goal-page layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,6 @@
 // // Javascript for the lower navigation carousel --> scroll prev and next
-
 // source: https://www.codeply.com/p/0CWffz76Q9
-let items = document.querySelectorAll('.carousel .carousel-item')
+let items = document.querySelectorAll('#carouselLowerNavigation .carousel-item')
 
 items.forEach((el) => {
 	const minPerSlide = 4
@@ -13,6 +12,34 @@ items.forEach((el) => {
 		}
 		let cloneChild = next.cloneNode(true)
 		el.appendChild(cloneChild.children[0])
+		next = next.nextElementSibling
+	}
+})
+
+
+// cards carousel on frontpage
+let width = screen.width;
+let cards = document.querySelectorAll('#carouselCard .carousel-item');
+let minSlide;
+
+// change number of cards per slide according to screen width
+if (width >= 1200) {
+	minSlide = 3;
+} else if (width >= 768 && width < 1200 ) {
+	minSlide = 2;
+} else {
+	minSlide = 1;
+}
+
+cards.forEach((cc) => {
+	let next = cc.nextElementSibling
+	for (var i=1; i<minSlide; i++) {
+		if (!next) {
+			// wrap carousel by using first child
+			next = cards[0]
+		}
+		let cloneChild = next.cloneNode(true)
+		cc.appendChild(cloneChild.children[0])
 		next = next.nextElementSibling
 	}
 })
